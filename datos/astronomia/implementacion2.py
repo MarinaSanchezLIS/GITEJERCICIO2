b1dfd9a73e56545db2466d5418afe4f42fd1a0e7
Marina Sanchez Sanchez <marina.sanchez@lisdatasolutions.com>
2026-05-28 16:04:43 +0200

    D implementa 2 con su nueva rama local

---
+++ datos/astronomia/implementacion2.py
@@ -0,0 +1,10 @@
+def explora(a):
+    sitios_a_no_explorar = ["los baños", "el granero", "la madriguera"]
+    print(f"el alien {a} no va a explorar {random.choice(sitios_a_no_explorar)}")
+
+def cosmos(a, p, g):
+    for alien in a:
+        print(
+            f"El {alien} del planeta {random.choice(p)} de la galaxia {random.choice(g)}"
+        )
+        explora(a)
